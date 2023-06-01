269bf358a5009416558220435918dd2658cfb476
fkielbasa <126203484+fkielbasa@users.noreply.github.com>
2023-06-01 18:01:52 +0200

    commit

--- main.py
+++ main.py
@@ -1,39 +1,49 @@
 import tkinter as tk
 from tkinter import filedialog
 from skimage.filters import threshold_otsu
-import numpy as np
+from skimage.measure import label
+import numpy
 from PIL import ImageTk, Image
 import cv2
+from matplotlib import pyplot as plt
+import numpy as np
 
 
 class App:
     def __init__(self, master):
         self.master = master
-        self.master.title("Segmentacja tekstury")
-        self.img_path = ""
-        self.img = None
-        self.bw_img = None
-        self.segmented_img = None
-        self.threshold = 0.5
+        self.master.title("Segmentacja tekstur")
 
         # Ustawienie tła na czarne
         self.master.configure(bg="black")
 
         # Tytuł
         self.title_label = tk.Label(
-            self.master, text="Segmentacja tekstury", bg="black", fg="white", font=("Arial", 16, "bold")
+            self.master, text="Segmentacja tekstur", font=("Helvetica", 16), bg="black", fg="white"
         )
-        self.title_label.pack(padx=10, pady=10)
+        self.title_label.pack(anchor="n", pady=10)
 
         # Ramka dla przycisku wyboru pliku
         self.select_frame = tk.Frame(self.master, bg="black")
-        self.select_frame.pack(anchor="nw", padx=10, pady=10)
+        self.select_frame.pack(anchor="n", pady=10)
 
         # Przycisk do wyboru pliku
         self.select_button = tk.Button(
             self.select_frame, text="Wybierz obraz", command=self.select_image
         )
-        self.select_button.pack(side="left")
+        self.select_button.pack(side="left", padx=10)
+
+        # Przycisk do zapisu segmentowanego obrazu
+        self.save_segmented_button = tk.Button(
+            self.select_frame, text="Zapisz segmentowany obraz", command=self.save_segmented_image
+        )
+        self.save_segmented_button.pack(side="left", padx=10)
+
+        # Przycisk do zapisu obrazu z maską
+        self.save_masked_button = tk.Button(
+            self.select_frame, text="Zapisz obraz z maską", command=self.save_masked_image
+        )
+        self.save_masked_button.pack(side="left", padx=10)
 
         # Suwak do ustawiania progu binaryzacji
         self.threshold_slider = tk.Scale(
@@ -50,13 +60,13 @@ class App:
             troughcolor="gray",
         )
         self.threshold_slider.set(self.threshold)
-        self.threshold_slider.pack(anchor="n", padx=10, pady=10)
+        self.threshold_slider.pack(anchor="n", pady=10)
 
         # Etykieta dla suwaka
         self.threshold_label = tk.Label(
             self.master, text="Próg binaryzacji:", bg="black", fg="white"
         )
-        self.threshold_label.pack(anchor="n", padx=10)
+        self.threshold_label.pack(anchor="n", pady=10)
 
         # Ramka dla obrazów
         self.image_frame = tk.Frame(self.master, bg="black")
@@ -74,20 +84,15 @@ class App:
         self.segmented_label = tk.Label(self.master, bg="black")
         self.segmented_label.pack(padx=10, pady=10)
 
-        # Przycisk do zapisu segmentowanego obrazu
-        self.save_segmented_button = tk.Button(
-            self.master, text="Zapisz segmentowany obraz", command=self.save_segmented_image
-        )
-        self.save_segmented_button.pack(padx=10, pady=5)
-
-        # Przycisk do zapisu obrazu z maską
-        self.save_masked_button = tk.Button(
-            self.master, text="Zapisz obraz z maską", command=self.save_masked_image
-        )
-        self.save_masked_button.pack(padx=10, pady=5)
-
         # Minimalna wielkość okna
         self.master.minsize(width=800, height=400)
+
+        # Inicjalizacja zmiennych
+        self.img_path = ""
+        self.img = None
+        self.bw_img = None
+        self.segmented_img = None
+        self.threshold = 0.5
 
     def select_image(self):
         # Wybieranie pliku z obrazem
@@ -108,82 +113,18 @@ class App:
             # Wczytywanie obrazu jako obiekt typu numpy array
             img = cv2.imread(self.img_path)
 
-            # Konwersja obrazu do skali szarości
-            gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
             # Zastosowanie binaryzacji z użyciem progu
-            ret, bw = cv2.threshold(
-                gray_img, int(self.threshold * 255), 255, cv2.THRESH_BINARY
+            ret, bw1 = cv2.threshold(
+                img, int(self.threshold * 255), 255, cv2.THRESH_BINARY
             )
 
             # Konwersja z obiektu typu numpy array do PIL Image
-            self.bw_img = Image.fromarray(bw)
+            self.bw_img = Image.fromarray(bw1)
             # Konwersja PIL Image do formatu, który może być wyświetlony w Tkinter
             self.bw_img = ImageTk.PhotoImage(self.bw_img)
             # Wyświetlanie zbinaryzowanego obrazu
             self.bw_label.configure(image=self.bw_img)
             self.bw_label.image = self.bw_img
-
-            # Tworzenie maski trzeciego obrazu
-            mask = cv2.inRange(bw, 0, 50)
-
-            # Zastosowanie maski na trzecim obrazie
-            masked_img = self.apply_mask(img, mask)
-
-            # Konwersja obrazu do formatu RGB
-            masked_img_rgb = cv2.cvtColor(masked_img, cv2.COLOR_BGR2RGB)
-
-            # Tworzenie obiektu PIL Image
-            masked_img_pil = Image.fromarray(masked_img_rgb)
-
-            # Konwersja PIL Image do formatu, który może być wyświetlony w Tkinter
-            masked_img_tk = ImageTk.PhotoImage(masked_img_pil)
-
-            # Wyświetlanie segmentowanego obrazu
-            self.segmented_label.configure(image=masked_img_tk)
-            self.segmented_label.image = masked_img_tk
-
-    def apply_mask(self, image, mask):
-        # Utworzenie obrazu o rozmiarze maski
-        mask_image = np.zeros_like(image)
-
-        # Ustawienie koloru maski na żółty (255, 255, 0)
-        mask_color = (255, 255, 0)
-
-        # Kopiowanie wartości pikseli z obrazu oryginalnego na obraz maski tylko dla wartości niezerowych w masce
-        mask_image[mask > 0] = mask_color
-
-        # Dodanie maski do obrazu oryginalnego
-        masked_image = cv2.addWeighted(image, 0.7, mask_image, 0.3, 0)
-
-        return masked_image
-
-    def update_threshold(self, value):
-        self.threshold = float(value)
-        # Segmentacja obrazu po zmianie progu
-        self.segment_image()
-
-    def save_segmented_image(self):
-        if self.segmented_img:
-            # Wybieranie miejsca zapisu segmentowanego obrazu
-            save_path = filedialog.asksaveasfilename(
-                title="Zapisz segmentowany obraz",
-                defaultextension=".png",
-                filetypes=(("PNG files", "*.png"), ("All files", "*.*")),
-            )
-            if save_path:
-                # Konwersja PhotoImage do Image
-                image_pil = self.segmented_img
-                image_pil = image_pil._PhotoImage__photo
-                image_pil = ImageTk.getimage(image_pil)
-
-                # Zapisywanie segmentowanego obrazu
-                image_pil.save(save_path)
-
-    def save_masked_image(self):
-        if self.img_path:
-            # Wczytywanie obrazu jako obiekt typu numpy array
-            img = cv2.imread(self.img_path)
 
             # Wykrycie pierwszej tekstury
             texture1_mask = cv2.inRange(img, (0, 0, 0), (50, 50, 50))
@@ -196,30 +137,56 @@ class App:
             label_matrix[texture1_mask == 255] = 1
             label_matrix[texture2_mask == 255] = 2
 
+            # Utworzenie dwóch oddzielnych obrazów dla każdej tekstury
+            texture1 = cv2.bitwise_and(img, img, mask=texture1_mask)
+            texture2 = cv2.bitwise_and(img, img, mask=texture2_mask)
+
             # Przypisanie kolorów do etykiet
             label_colored = np.zeros_like(img)
             label_colored[label_matrix == 1] = [0, 255, 255]  # Kolor żółty
             label_colored[label_matrix == 2] = [255, 0, 0]  # Kolor niebieski
 
             # Nałożenie kolorów na obrazy tekstur
-            texture1_colored = cv2.addWeighted(img, 0.5, label_colored, 0.5, 0)
-            texture2_colored = cv2.addWeighted(img, 0.5, label_colored, 0.5, 0)
+            texture1_colored = cv2.addWeighted(texture1, 0.5, label_colored, 0.5, 0)
+            texture2_colored = cv2.addWeighted(texture2, 0.5, label_colored, 0.5, 0)
 
-            result = cv2.add(texture1_colored, texture2_colored)
-            result2 = cv2.cvtColor(result, cv2.COLOR_BGR2RGB)
+            # Połączenie obrazów tekstur w jeden obraz segmentowany
+            self.segmented_img = texture1_colored + texture2_colored
 
-            # Konwersja obrazu do formatu PIL Image
-            result_pil = Image.fromarray(result2)
+            # Konwersja z obiektu typu numpy array do PIL Image
+            self.segmented_img = Image.fromarray(self.segmented_img)
+            # Konwersja PIL Image do formatu, który może być wyświetlony w Tkinter
+            self.segmented_img = ImageTk.PhotoImage(self.segmented_img)
+            # Wyświetlanie segmentowanego obrazu
+            self.segmented_label.configure(image=self.segmented_img)
+            self.segmented_label.image = self.segmented_img
 
-            # Wybieranie miejsca zapisu obrazu z maską
+    def save_segmented_image(self):
+        if self.segmented_img:
+            # Zapisywanie segmentowanego obrazu
             save_path = filedialog.asksaveasfilename(
-                title="Zapisz obraz z maską",
-                defaultextension=".png",
-                filetypes=(("PNG files", "*.png"), ("All files", "*.*")),
+                title="Zapisz segmentowany obraz", defaultextension=".jpg"
             )
             if save_path:
+                self.segmented_img.save(save_path)
+
+    def save_masked_image(self):
+        if self.bw_img:
+            # Zapisywanie obrazu z maską
+            save_path = filedialog.asksaveasfilename(
+                title="Zapisz obraz z maską", defaultextension=".jpg"
+            )
+            if save_path:
+                # Konwersja z PIL Image do obiektu typu numpy array
+                masked_img = np.array(self.bw_img)
                 # Zapisywanie obrazu z maską
-                result_pil.save(save_path)
+                cv2.imwrite(save_path, masked_img)
+
+    def update_threshold(self, value):
+        self.threshold = float(value)
+        # Segmentacja obrazu przy aktualnym progu
+        self.segment_image()
+
 
 root = tk.Tk()
 app = App(root)
